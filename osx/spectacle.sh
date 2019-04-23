3c95c972ca6301ea99533c8e19d15a615230c32f
Nick Badal <me@nbad.al>
2019-04-22 17:41:30 -0700

    Fix spectacle LN direction

--- osx/spectacle.sh
+++ osx/spectacle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm ~/Library/Application\ Support/Spectacle/Shortcuts.json
-ln -sf ~/Library/Application\ Support/Spectacle/Shortcuts.json ~/dotfiles/osx/spectacle.json
+rm -f ~/Library/Application\ Support/Spectacle/Shortcuts.json
+ln -sf ~/dotfiles/osx/spectacle.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
